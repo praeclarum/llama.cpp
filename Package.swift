ea188a6d05f5f524c1e716c60335abdb9d70c99a
Frank A. Krueger <fak@praeclarum.org>
2023-11-11 09:15:28 -0800

    Enable ACCELERATE_NEW_LAPACK

--- Package.swift
+++ Package.swift
@@ -53,11 +53,9 @@ let package = Package(
             cSettings: [
                 .unsafeFlags(["-Wno-shorten-64-to-32", "-O3", "-DNDEBUG"]),
                 .define("GGML_USE_ACCELERATE")
-                // NOTE: NEW_LAPACK will required iOS version 16.4+
-                // We should consider add this in the future when we drop support for iOS 14
-                // (ref: ref: https://developer.apple.com/documentation/accelerate/1513264-cblas_sgemm?language=objc)
-                // .define("ACCELERATE_NEW_LAPACK"),
-                // .define("ACCELERATE_LAPACK_ILP64")
+                // NOTE: NEW_LAPACK requires iOS version 16.4+
+                .define("ACCELERATE_NEW_LAPACK"),
+                .define("ACCELERATE_LAPACK_ILP64")
             ] + additionalSettings,
             linkerSettings: [
                 .linkedFramework("Accelerate")
